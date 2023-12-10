04933b18d212be933fc3255fd27418c79ceb9538
egpennington (애멧) <53251137+egpennington@users.noreply.github.com>
2023-12-10 11:06:02 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 function playSong(videoId) {
     console.log("song playing")
-    const player = document.getElementById('player')
-    player.src = 'https://www.youtube.com/embed/' + videoId + '?autoplay=1'
+    const player = document.getElementById("player")
+    player.src = "https://www.youtube.com/embed/" + videoId + "?autoplay=1"
 }
